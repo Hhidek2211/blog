c3ca3faa2a180a0b3c5046743f998c7d596930df
Hhidek2211 <DevHhide1011@gmail.com>
2023-07-01 16:14:56 +0900

    課題8-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -17,4 +18,15 @@ class PostController extends Controller
         return view('Posts.show') -> with(['post' => $post]);
     }
     
+    public function create()
+    {
+        return view('Posts.create');
+    }
+    
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post -> fill($input) -> save();
+        return redirect('/posts/' . $post -> id );
+    }
 }
