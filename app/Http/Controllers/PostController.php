9282418407b434ce4d862900c3153592eed5f354
Hhidek2211 <DevHhide1011@gmail.com>
2023-07-06 15:12:46 +0900

    課題9-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
@@ -18,9 +19,9 @@ class PostController extends Controller
         return view('Posts.show') -> with(['post' => $post]);
     }
     
-    public function create()
+    public function create(Category $category)
     {
-        return view('Posts.create');
+        return view('Posts.create') -> with(['categories' => $category -> get() ]);
     }
     
     public function store(Post $post, PostRequest $request)
